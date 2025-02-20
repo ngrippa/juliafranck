5733dc14591c5378535a48a9d34fe5618cbdb64d
Nemo Grippa <nemo.grippa@gmail.com>
2025-02-20 08:25:19 +0100

    add site

---
+++ assets/js/js.js
@@ -0,0 +1,39 @@
+
+(function () {
+
+    const jf = (function () {
+        "use strict";
+
+	      function encodeEmails() {
+	      	const mails = document.querySelectorAll('.mail');
+	      	mails.forEach((m)=> {
+	      		const target = m.dataset.user || 'info';
+	      		const domain = m.dataset.domain || 'landwehr-cie.de';
+	      		const link = `${target}@${domain}`;
+	      		const text = m.textContent || link;
+	      		const a = document.createElement('a');
+	      		a.href = `mailto:${link}`;
+	      		a.textContent = text;
+	      		m.append(a);
+	      	});
+	      }
+
+	      const initModule = function () {
+	        encodeEmails();
+        };
+        // End public method /initModule/
+
+        // return public methods
+        return {
+            initModule: initModule
+        };
+        //------------------- END PUBLIC METHODS ---------------------
+    }());
+
+    if (document.readyState === 'loading') {
+        document.addEventListener('DOMContentLoaded', jf.initModule);
+    } else {  // `DOMContentLoaded` has already fired
+        jf.initModule();
+    }
+
+})();
